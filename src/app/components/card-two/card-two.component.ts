5598433705e32b5387b2a04ecf870e7ad938abaf
ahmedbadawi.1411 <ahmedbadawi.1411@gmail.com>
2025-03-26 10:27:42 +0200

    news

--- src/app/components/card-two/card-two.component.ts
+++ src/app/components/card-two/card-two.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-card-two',
@@ -7,8 +8,13 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrl: './card-two.component.css'
 })
 export class CardTwoComponent {
+  constructor(private apiService:ApiService) {}
+  
   @Output() viewEmmiter = new EventEmitter<any>()
+  @Input() data: any = {};
+
   onClick() {
+    this.apiService.selectedObject=this.data;
     this.viewEmmiter.emit({page:'blog'})
   }
 }
