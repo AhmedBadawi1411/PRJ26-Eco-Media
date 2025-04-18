80e4f949608b1be8fffc6ab85d205929312a3c07
ahmed badawi <ahmedbadawi.1411@gmail.com>
2025-04-18 21:44:59 +0200

    maintainance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomePageComponent } from "./pages/home-page/home-page.component";
 import { ApiService } from './services/api.service';
+import { MaintainenceComponent } from "./pages/maintainence/maintainence.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomePageComponent],
+  imports: [RouterOutlet, HomePageComponent, MaintainenceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
