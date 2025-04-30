5f2d22be22369c1b7edaf5f922e2b807b1176780
ahmed badawi <ahmedbadawi.1411@gmail.com>
2025-04-30 17:00:11 +0300

    reports

--- src/app/pages/aboutus/aboutus.component.ts
+++ src/app/pages/aboutus/aboutus.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { VideoPlayerComponent } from "../../components/video-player/video-player.component";
 
 @Component({
   selector: 'app-aboutus',
-  imports: [],
+  imports: [VideoPlayerComponent],
   templateUrl: './aboutus.component.html',
   styleUrl: './aboutus.component.css'
 })
